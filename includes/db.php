8d0a8cb1e1646c189615a9a2975c2836e39e219a
Jma777 <jmamorizot@aol.fr>
2015-07-23 11:28:47 +0200

    First Commit
    Tous les dossier  de base du tagged_twitetr

---
+++ includes/db.php
@@ -0,0 +1,21 @@
+<?php
+
+    define("DBHOST", "localhost"); 	// IP du serveur
+    define("DBUSER", "root");		// UserName MySQL
+    define("DBPASS", "");			// MDP MySQL
+    define("DBNAME", "bucket_list");// Nom de la BDD
+
+	try {
+		$dbh = new PDO(
+			'mysql:host='.DBHOST.';dbname='.DBNAME, 
+			DBUSER, 
+			DBPASS, 
+			array(
+				PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+			)
+		);
+	} catch (PDOException $e) {
+	    echo 'Erreur de connexion : ' . $e->getMessage();
+	}
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<script type="text/javascript" src="js/jQuery.js"></script>
+	<link rel="stylesheet" href="css/font-awesome/css/font-awesome.css">
+	<link href="css/demo-page.css" rel="stylesheet" media="all">
+	<link href="css/hover.css" rel="stylesheet" media="all">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
