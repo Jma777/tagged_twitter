e74c28e920bfd5285dc3597c9d1496678bf69488
NicolasDelille <nicolas.delille@gmail.com>
2015-07-27 15:41:12 +0200

    update SQL request and add link

--- getTagAndRelatedTweets.php
+++ getTagAndRelatedTweets.php
@@ -16,14 +16,16 @@ include("includes/functions.php");
 	$tag_id = $tag['id'];
 
 	// pr($tag);
-	echo "<h2>".$tag['title']."</h2>";
+	echo "<h2>".'<a href="showTweetsByTag.php?i='.$tag_id.'">'.$tag['title'].'</a>'."</h2>";
 	echo "<br />";
 
 
 
 // affichage des tweets
-	$sql = "SELECT *
-			FROM tweets
+	$sql = "SELECT t.id, t.user_id, t.tag_id, t.tweet_content, t.link, t.pic, t.date_created, users.user_name
+			FROM tweets as t
+			JOIN users
+			ON t.user_id = users.id
 			WHERE tag_id = :tag_id
 			ORDER BY date_created DESC
 			LIMIT 10";
@@ -32,6 +34,7 @@ include("includes/functions.php");
 	$sth->bindValue(":tag_id",$tag_id);
 	$sth->execute();
 	$tweets = $sth->fetchAll();
+	pr($tweets);
 
 	
 	
@@ -42,7 +45,10 @@ include("includes/functions.php");
 	 	$img = $tweet['pic'] . ".jpg";
 	 	$img_title = $tweet['pic'];
 	 	$link = $tweet['link'];
-	 	echo "<p>".$tweet_content."</p>";
+	 	$tweet_author = $tweet['user_name'];
+	 	$tweet_date = $tweet['date_created'];
+	 	$id = $tweet['user_id'];
+	 	echo "<p>".$tweet_content.'<br />'.'<a href="showTweetsByProfile.php?i='.$id.'">'.$tweet_author.'</a>'.'<br />'."posté le : ".$tweet_date."</p>";
 	}
 
 	// pr($tweets);
